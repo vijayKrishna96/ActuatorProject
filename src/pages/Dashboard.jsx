76cf8b75aa3bdc51ed7a5dba3f94e44ab4b5b5be
Punya-d-123 <punyadonnie@gmail.com>
2025-05-28 11:22:51 +0400

    second commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+function Dashboard(){
+    return(
+<div>
+    <h1>Project</h1>
+</div>
+    )
+}
+export default Dashboard
--- src/pages/demopage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function demopage() {
-  return (
-    <div>demopage</div>
-  )
-}
-
-export default demopage
