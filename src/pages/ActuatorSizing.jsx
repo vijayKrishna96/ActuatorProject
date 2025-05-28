30831884673dbed385e2310d2c9c74b5d5de5327
Joe <joedonnie07@gmail.com>
2025-05-28 16:11:24 +0530

    modified components

---
+++ src/pages/ActuatorSizing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ActuatorSizing() {
+    return(
+        <div>
+            <h1>Actuator Sizing</h1>
+        </div>
+    )
+}
+
+export default ActuatorSizing;
